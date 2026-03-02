37e57c8b75a8facbd993ffdb73d3810aad7b6516
rhv1501 <rhv4748@gmail.com>
2026-03-03 00:18:37 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,7 +104,9 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
+      >
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-600 text-white px-4 py-2 rounded-md z-50"
