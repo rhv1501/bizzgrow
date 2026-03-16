a050bc34b3848d7f3062443141a5ace3f7072cdd
rhv1501 <rhv4748@gmail.com>
2026-03-17 01:30:03 +0530

    robots.ts update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://bizzgrow.com'
+  const baseUrl = 'https://bizzgrowlabs.com'
   
   return {
     rules: {
